93fba78c09899303e7be988e0e35345776dae99a
admin <827567696@qq.com>
2021-12-02 12:00:19 +0800

    Initial commit

---
+++ IDE1/16.字符串格式化2.py
@@ -0,0 +1,6 @@
+# print ('%s is easy to learn' % 'Python')
+# print("%s's score is %d" % ('Mike', 87))
+
+name = 'Lily'
+score = 95
+print("%s's score is %d" % (name, score))
---
+++ IDE1/18.bool类型转换.py
@@ -0,0 +1,9 @@
+bool('False')
+
+a = '123'
+if a:
+    print('this is not a blank string')
+
+
+    if bool(a) == True:
+        if a !='':

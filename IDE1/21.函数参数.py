e3267b4d6bcefe702eafc5c61045d69d66e13472
admin <827567696@qq.com>
2021-12-02 11:40:57 +0800

    Initial commit

---
+++ IDE1/21.函数参数.py
@@ -0,0 +1,15 @@
+'''
+在19课里，我们讲了怎样定义一个自己的函数，但我们没有给他提供输入参数的功能。不能指定参数的函数就好比你去餐厅吃饭，
+服务员告诉你，不能点菜，有啥吃啥。这显然不能满足很多情况
+'''
+# def sayHello(someone):
+#     print(sonemone + 'says Hello！')
+
+# def plus(num1,num2):
+#     print(num1+num2)
+
+
+x = 3
+y = 4
+plus=x+y
+print(plus)

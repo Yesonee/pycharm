43ad25fcdc2477227d647422f22999577bdee6c8
admin <827567696@qq.com>
2021-12-02 12:16:04 +0800

    Initial commit

--- IDE1/22.函数应用示例.py
+++ IDE1/22.函数应用示例.py
@@ -1,3 +1,22 @@
+# def isEqual(num1,num2):
+#     if num1<num2:
+#         print('too small')
+#         return False
+#     if num1>num2:
+#         print('too big')
+#         return False
+#     if num1==num2:
+#         print('bingo!')
+#         return True
+
+# from random import randint
+# num = randint(1, 100)
+# print('Guess what I think?')
+# bingo = False
+# while bingo == False:
+#    answer = int(input())
+#    bingo = isEqual(answer, num)
+
 def isEqual(num1,num2):
     if num1<num2:
         print('too small')
@@ -6,5 +25,12 @@ def isEqual(num1,num2):
         print('too big')
         return False
     if num1==num2:
-        print('bingo!')
+        print('bingo')
         return True
+from  random import  randint
+num = randint(1,100)
+print('Guess what I think?')
+bingo = False
+while bingo == False:
+    answer = int(input())
+    bingo = isEqual(answer,num)

ceb9034cdd29b396d9b789f7c4b0cec46f3fb53c
admin <827567696@qq.com>
2021-12-02 17:16:44 +0800

    Initial commit

---
+++ IDE1/25.初探list.py
@@ -0,0 +1,15 @@
+# for i in range(1,10):
+#     print(i)
+#
+# print(list(range(1,12589)))
+
+
+
+# 1 = range(1,10)
+# for i in 1:
+#     print(1)
+#
+# l = ['meat', 'egg', 'fish', 'milk']
+#
+# l = [365, 'everyday', 0.618, True]
+

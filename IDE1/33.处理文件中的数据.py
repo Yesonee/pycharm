54ce29b052d26ec6176de443c0ca20c8f0f30b0e
admin <827567696@qq.com>
2021-12-03 11:31:41 +0800

    Initial commit

--- IDE1/33.处理文件中的数据.py
+++ IDE1/33.处理文件中的数据.py
@@ -7,8 +7,9 @@ f.close()
 results = []
 
 for line in lines:
+    #print (line)
     data = line.split()
-    print(data)
+    #print (data)
 
 '''
 4.整个程序最核心的部分到了。如何把一个学生的几次成绩合并，并保存起来呢？
@@ -22,17 +23,17 @@ for line in lines:
 score是一个字符串，为了做计算，需要转成整数值int。
 result中，我加了一个制表符\t和换行符\n，让输出的结果更好看些。
 """
-sum = 0
-score_list = data[1:] # 学生各门课的成绩列表
-for score in score_list:
-    sum  += int(score)
-result = '%s\t: %d\n' %(data[0],sum) # 名字和总分
+    sum = 0
+    #score_list = data[1:] # 学生各门课的成绩列表
+    for score in data[1:]:
+        point = int (score)
+        sum +=  int (score)
+    result = '%s\t: %d\n' %(data[0],sum) # 名字和总分
+    #print(result)
 
-print(result)
-results.append(result)
+    results.append(result)
 
 #print(results)
-
 output = open('result.txt','w',encoding='UTF-8')
 output.writelines(results)
 output.close()

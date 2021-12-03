d1e311c4bc9178da462715d4c8547a83dadea061
admin <827567696@qq.com>
2021-12-03 18:05:35 +0800

    Initial commit

--- IDE1/35.continue.py
+++ IDE1/35.continue.py
@@ -1,5 +1,20 @@
-for score in data[1:]
-    point = int(score)
-    if point < 60:
-        continue
-    sum += point
+# for score in data[1:]
+#     point = int(score)
+#     if point < 60:
+#         continue
+#     sum += point
+
+i = 0
+while i< 5:
+    i += 1
+    for j in range(3):
+        print (j)
+        if j ==2:
+            break
+        for k in range(3):
+            if k == 2:
+                continue
+            print(k)
+        if i > 3:
+            break
+        print(i)

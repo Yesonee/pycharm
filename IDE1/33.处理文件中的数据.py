1cd16b6fe5bf9756e43da0ac07d08b136e5f0e47
admin <827567696@qq.com>
2021-12-03 16:07:22 +0800

    Initial commit

--- IDE1/33.处理文件中的数据.py
+++ IDE1/33.处理文件中的数据.py
@@ -1,15 +1,16 @@
 f = open('scores.txt',encoding='utf-8')
 #取得文件中的数据。因为每一行都是一条学生成绩的记录，所以用readlines
 lines =f.readlines()
+print(lines)
 f.close()
-#print(lines)
-#对每一条数据进行处理。按照空格，把姓名、每次的成绩分割开
-results = []
 
+
+results = []
 for line in lines:
+    # 对每一条数据进行处理。按照空格，把姓名、每次的成绩分割开
     #print (line)
     data = line.split()
-    #print (data)
+    print (data)
 
 '''
 4.整个程序最核心的部分到了。如何把一个学生的几次成绩合并，并保存起来呢？
@@ -23,15 +24,17 @@ for line in lines:
 score是一个字符串，为了做计算，需要转成整数值int。
 result中，我加了一个制表符\t和换行符\n，让输出的结果更好看些。
 """
-    sum = 0
-    #score_list = data[1:] # 学生各门课的成绩列表
-    for score in data[1:]:
+
+
+sum = 0
+#学生各门课的成绩列表
+for score in data[1:]:
         point = int (score)
         sum +=  int (score)
-    result = '%s\t: %d\n' %(data[0],sum) # 名字和总分
-    #print(result)
+result = '%s\t: %d\n' %(data[0],sum) # 名字和总分
+print(result)
 
-    results.append(result)
+results.append(result)
 
 #print(results)
 output = open('result.txt','w',encoding='UTF-8')
---
+++ IDE1/34.break.py
@@ -0,0 +1,13 @@
+while True:
+    a = input()
+    if a == 'EOF':
+        break
+
+for i  in range(10):
+    a = input()
+    if a == 'EOF':
+        break
+
+if answer < 0:
+    print('Exit game...')
+    break

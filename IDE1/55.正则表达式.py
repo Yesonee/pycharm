6858b2369267550899bca87bb88313e83dc5f491
admin <827567696@qq.com>
2021-12-10 18:04:09 +0800

    Initial commit

--- IDE1/55.正则表达式.py
+++ IDE1/55.正则表达式.py
@@ -10,8 +10,8 @@ def regexFlag():
     演示re模块常量的使用
     :return:
     '''
-    #1 re
-    test    = 'Yeson大帅比b'
+    # 1 re
+    test = 'Yeson大帅比b'
     pattern = r'Yeson大帅比B'
-    print('默认模式',re.findall(pattern,test))
-    print('忽略大小写 模式', re.findall(pattern, test,re.IGNORECASE))
+    print('默认模式', re.findall(pattern, test))
+    print('忽略大小写 模式', re.findall(pattern, test, re.IGNORECASE))
